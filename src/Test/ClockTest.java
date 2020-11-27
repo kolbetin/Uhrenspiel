9dac74e731f55b35de1b2a3cff0845b51ca48952
Oliver Piert <piertoli@students.zhaw.ch>
2020-11-27 17:04:29 +0100

    Koordinaten hinzugefügt

--- src/Test/ClockTest.java
+++ src/Test/ClockTest.java
@@ -118,63 +118,62 @@ public class ClockTest extends Application {
         outerCircle.toBack();
         Circle innerCircle = new Circle(300,300,5, Color.BLACK);
         innerCircle.toFront();
-
         // Drawing a Circle12 and registering the event handler
         Circle circle12 = new Circle(300, 165, 25, Color.RED);
         Text nummer12 = new Text(285,175,"12");
         nummer12.setFont(nummerFont);
         circle12.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle1 and registering the event handler
+        // Drawing a Circle1 and registering the event handler
         Circle circle1 = new Circle(366, 182, 25, Color.GREEN);
         Text nummer1 = new Text(358,192,"1");
         nummer1.setFont(nummerFont);
         circle1.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle2 and registering the event handler
+        // Drawing a Circle2 and registering the event handler
         Circle circle2 = new Circle(418, 234, 25, Color.GREEN);
         Text nummer2 = new Text(410,244,"2");
         nummer2.setFont(nummerFont);
         circle2.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle3 and registering the event handler
+        // Drawing a Circle3 and registering the event handler
         Circle circle3 = new Circle(435, 300, 25,Color.RED);
         Text nummer3 = new Text(427,310,"3");
         nummer3.setFont(nummerFont);
         circle3.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle4 and registering the event handler
+        // Drawing a Circle4 and registering the event handler
         Circle circle4 = new Circle(418, 366, 25,Color.GREEN);
         Text nummer4 = new Text(410,376,"4");
         nummer4.setFont(nummerFont);
         circle4.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle5 and registering the event handler
+        // Drawing a Circle5 and registering the event handler
         Circle circle5 = new Circle(366, 418, 25,Color.GREEN);
         Text nummer5 = new Text(358,428,"5");
         nummer5.setFont(nummerFont);
         circle5.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle6 and registering the event handler
+        // Drawing a Circle6 and registering the event handler
         Circle circle6 = new Circle(300, 435, 25,Color.RED);
         Text nummer6 = new Text(292,445,"6");
         nummer6.setFont(nummerFont);
         circle6.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle7 and registering the event handler
+        // Drawing a Circle7 and registering the event handler
         Circle circle7 = new Circle(234, 418, 25,Color.GREEN);
         Text nummer7 = new Text(226,428,"7");
         nummer7.setFont(nummerFont);
         circle7.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle8 and registering the event handler
+        // Drawing a Circle8 and registering the event handler
         Circle circle8 = new Circle(182, 366, 25,Color.GREEN);
         Text nummer8 = new Text(174,376,"8");
         nummer8.setFont(nummerFont);
         circle8.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle9 and registering the event handler
+        // Drawing a Circle9 and registering the event handler
         Circle circle9 = new Circle(165, 300, 25,Color.RED);
         Text nummer9 = new Text(157,310,"9");
         nummer9.setFont(nummerFont);
         circle9.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle10 and registering the event handler
+        // Drawing a Circle10 and registering the event handler
         Circle circle10 = new Circle(182, 234, 25,Color.GREEN);
         Text nummer10 = new Text(167,244,"10");
         nummer10.setFont(nummerFont);
         circle10.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-// Drawing a Circle11 and registering the event handler
+        // Drawing a Circle11 and registering the event handler
         Circle circle11 = new Circle(234, 182, 25,Color.GREEN);
         Text nummer11 = new Text(219,192,"11");
         nummer11.setFont(nummerFont);
