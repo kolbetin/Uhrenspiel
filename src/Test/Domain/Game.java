34ee0c1d88e3a1812069a79a02d576c1553f1e11
Tina Kolbe <kolbetin@students.zhaw.ch>
2020-11-30 16:32:57 +0100

    Struktur eingefügt

--- src/Test/Domain/Game.java
+++ src/Test/Domain/Game.java
@@ -9,10 +9,11 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import Test.Presentation.AlertHelper;
-import java.util.List;
-import java.util.Random;
+
+import java.util.*;
 
 
 public class Game extends GUI {
@@ -23,13 +24,23 @@ public class Game extends GUI {
         private Random random;
         public List playedGames;
         private IOSerialisierung ioSerialisierung;
+        public ArrayList<Integer> liste;
+        public List answers;
+        private int key;
+
+
 
         public Game(){
             progressData = new ProgressData();
             questionsAnswermap = new QuestionsAnswer();
-            //game();
-
-        }
+            random = new Random();
+            liste =  new ArrayList<Integer>();
+            System.out.println(questionsAnswermap.antwortenMap.size());
+            taskkey();
+            randomkey();
+            answerSet();
+            System.out.println(answers);
+            }
 
 
             @Override
@@ -47,17 +58,17 @@ public class Game extends GUI {
                 return borderPane;
             }
 
-            @Override
+             @Override
                 public Pane answerArea () {
                 final HBox hBox = new HBox(35);
                 hBox.setId("answerArea");
 
                 hBox.setPadding(new Insets(15, 10, 10, 50));
 
-                antwort1 = new Button(String.valueOf(questionsAnswermap.answers.get(0)));
-                antwort2 = new Button(String.valueOf(questionsAnswermap.answers.get(1)));
-                antwort3 = new Button(String.valueOf(questionsAnswermap.answers.get(2)));
-                antwort4 = new Button(String.valueOf(questionsAnswermap.answers.get(3)));
+                antwort1 = new Button(String.valueOf(answers.get(0)));
+                antwort2 = new Button(String.valueOf(answers.get(1)));
+                antwort3 = new Button(String.valueOf(answers.get(2)));
+                antwort4 = new Button(String.valueOf(answers.get(3)));
 
 
                 Text antwort = new Text("Antwort");
@@ -66,7 +77,50 @@ public class Game extends GUI {
                 return hBox;
             }
 
+    public int taskkey() {
 
+        for( int i = 0; liste.size() <2; i++){
+            int zufall = random.nextInt(questionsAnswermap.antwortenMap.size());
+            if (zufall != 0) {
+                key = zufall;
+                liste.add(key);
+            }
+        }
+
+        return key;
+    }
+
+    private Integer getkey()
+    {
+        for (Integer key : questionsAnswermap.antwortenMap.keySet()){
+            return key;
+        }
+        return null;
+    }
+
+    public List randomkey() {
+        int zufall;
+
+        for (int i = 0; liste.size() <4; i++) {
+            zufall = random.nextInt(questionsAnswermap.antwortenMap.size());
+            if (!liste.contains(zufall) & zufall!= 0 & zufall!= key) {
+                liste.add(zufall);
+            }
+        }
+
+        return null;
+
+    }
+
+    public void answerSet() {
+        answers = new ArrayList<String>();
+
+        for (int i = 0; i < 4; i++) {
+            String value =questionsAnswermap.antwortenMap.get(liste.get(i));
+            answers.add(value);
+            Collections.shuffle(answers);
+        }
+    }
 
 
 
--- src/Test/Domain/QuestionsAnswer.java
+++ src/Test/Domain/QuestionsAnswer.java
@@ -17,30 +17,21 @@ public class QuestionsAnswer extends Application {
     public HashMap<Line, Line>  uhrzeit;
     public HashMap<HashMap,String>  antworten;
     private ProgressData progressData;
-    public ArrayList<Integer> liste;
-    public List answers;
-    private int key;
 
-    private Random random;
+
+
+
 
     public void start(Stage primarystage){
 
     }
 
     public QuestionsAnswer() {
-        random = new Random();
-        liste =  new ArrayList<Integer>();
         antwortenMap = new HashMap<>();
         antworten = new HashMap<>();
         uhrzeit = new HashMap<>();
 
 
-      // uhrzeit.put(clock.stunde_1,clock.minuten_volleStunde);
-        //uhrzeit.put(clock.stunde_2,clock.minuten_volleStunde);
-       // add(uhrzeit,"1 Uhr");
-       // add(uhrzeit,"2 Uhr");
-      //  System.out.println(antworten);
-    //    System.out.println(uhrzeit.size());
         antwortenMapVolleStunde();
         //System.out.println(antwortenMap.get(3));
        // System.out.println(getKey());
@@ -48,23 +39,6 @@ public class QuestionsAnswer extends Application {
        // System.out.println(antwortenMap);
       // listebefüllen();
 
-      //  System.out.println(antwortenMap.size());
-       taskkey();
-    //    System.out.println(taskkey());
-        randomkey();
-      //  System.out.println(liste);
-        answerSet();
-        //gameset();
-
-        System.out.println(key);
-        System.out.println(liste);
-        System.out.println(answers);
-
-
-
-        //progressData.add(antwortenMap);
-       // System.out.println(antwortenMap.size());
-       // generateRandom(1,12);
     }
 
     private void antwortenMapVolleStunde() {
@@ -83,30 +57,6 @@ public class QuestionsAnswer extends Application {
         antwortenMap.put(12,"12");
     }
 
-
-
-
-  /*  public void gameSet() {
-        List count = new ArrayList<Integer>();
-        for (int i = 0; playedGames.size() < 11; i++) {
-            for (Integer key : antwortenMap.keySet()) {
-                if (!playedGames.contains(key) & key != 0) {
-                    playedGames.add(key);
-                }
-                count.add(i);
-            }
-        }
-    }*/
-
-    private Integer getkey()
-    {
-        for (Integer key : antwortenMap.keySet()){
-            return key;
-        }
-        return null;
-    }
-
-
     public String getValue(Integer value){
      return antwortenMap.get(value);
     }
@@ -114,58 +64,6 @@ public class QuestionsAnswer extends Application {
     public void add(HashMap Uhrenbild, String answer){
         antworten.put(Uhrenbild,answer);
     }
-
-  /*  public void gameset(){
-        taskkey();
-      randomkey(taskkey());
-       answerSet();
-
-    }*/
-
-
-    public int taskkey() {
-
-        for( int i = 0; liste.size() <2; i++){
-            int zufall = random.nextInt(antwortenMap.size());
-            if (zufall != 0) {
-                key = zufall;
-                liste.add(key);
-            }
-        }
-
-        return key;
-    }
-
-
-
-   public List randomkey() {
-       int zufall;
-
-        for (int i = 0; liste.size() <4; i++) {
-            zufall = random.nextInt(antwortenMap.size());
-            if (!liste.contains(zufall) & zufall!= 0 & zufall!= key) {
-                liste.add(zufall);
-            }
-        }
-
-        return null;
-
-        }
-
-    public void answerSet() {
-        answers = new ArrayList<String>();
-
-        for (int i = 0; i < 4; i++) {
-            String value =antwortenMap.get(liste.get(i));
-            answers.add(value);
-            Collections.shuffle(answers);
-        }
-
-
-    }
-
-
-
 
     public static void main(String[] args) {
         launch(args);
