df71b918c54c65af2881c2a537d46c9a47486d15
Tina Kolbe <kolbetin@students.zhaw.ch>
2020-12-07 19:21:27 +0100

    Uhrenspiel bearbeitet

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,8 +2,19 @@ package Test;
 import Test.Domain.Game;
 import Test.Presentation.ClockSkin;
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,28 +22,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import Test.Domain.ProgressData;
 
 /**
  * @author Crunchify.com
  *
  */
 
-public class Test extends Application {
-   private ClockSkin clockSkin;
-   private Game game;
-   public Node node;
+public class Test {
+  private ProgressData progressData;
 
 
-
-   public Node Test(){
-       game = new Game();
-       game.nextQuestion();
-       clockSkin = new ClockSkin(game.key);
-       node = clockSkin.createClock();
-       return node;
-   }
-
-    public void start(Stage primarystage) {}
-    public static void main(String[] args) { launch(args);    }
-
-}
+    }
