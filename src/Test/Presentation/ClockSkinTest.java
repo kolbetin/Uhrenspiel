2c312c35860270db375a2338584c26478717f698
Oliver Piert <piertoli@students.zhaw.ch>
2020-12-14 20:28:04 +0100

    Lernmodus enhancements

--- src/Test/Presentation/ClockSkinTest.java
+++ src/Test/Presentation/ClockSkinTest.java
@@ -1,51 +1,55 @@
 package Test.Presentation;
-import javafx.scene.Node;
+import javafx.scene.shape.Line;
 import org.junit.*;
 
 public class ClockSkinTest {
 
     ClockSkin clock = new ClockSkin();
-    String anzuzeigendeZeit = "01:15";
+    ClockElements clockElements = new ClockElements();
+    String anzuzeigendeZeit = "10:30";
 
-        @BeforeClass        // Executed once, before testing starts
+        /*@BeforeClass        // Executed once, before testing starts
         public static void beforeClass (){
-            System.out.println("I'm beforeClass method");
+            System.out.println("I'm before Class method");
+
         }
 
         @Before     // Executed before each test
         public void before (){
-            System.out.println("I'm before method");
-        }
+            System.out.println("I'm before each Test method");
+        }*/
 
         @Test       // Stunden-Parser Method Test
         public void stundenParserTest (){
-
         clock.createClock(anzuzeigendeZeit);
-        Assert.assertEquals(01, clock.stunde);
+        boolean sameLine = false;
+
+            if (clock.stunde == clock.parserStunde("10:30")) {
+                sameLine = true;
+            }
+            else{
+                sameLine = false;
+            }
+        Assert.assertTrue(sameLine);
         }
 
         @Test       // Minuten-Parser Method Test
         public void minutenParserTest (){
 
             clock.createClock(anzuzeigendeZeit);
-            Assert.assertEquals(15, clock.minute);
+            Assert.assertEquals(clock.minute, clock.parserMinuten("30"));
         }
 
 
 
-        @Test     // Test Case 2
-        public void test2 (){
-            System.out.println("I'm Test Case 2 method");
-        }
-
-        @After      // Executed after each test
+        /*@After      // Executed after each test
         public void after(){
-            System.out.println("I'm after method");
+            System.out.println("I'm before each Test method");
         }
 
         @AfterClass     // Executed once, at the end of testing
         public static void afterClass(){
             System.out.println("I'm after Class method");
-        }
+        }*/
 
-}
+} // Ende Test Klasse
