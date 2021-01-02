c8b9e0b1b2914c2685713f6e4723d716a6470637
Tina Kolbe <kolbetin@students.zhaw.ch>
2021-01-02 11:55:13 +0100

    Bild in ChoiceScreenLernmodus hinzugefügt

--- src/Test/Presentation/ChoiceScreenLernmodus.java
+++ src/Test/Presentation/ChoiceScreenLernmodus.java
@@ -1,12 +1,18 @@
 package Test.Presentation;
 
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
+import java.io.FileInputStream;
 
 
 public class ChoiceScreenLernmodus extends MainGUI {
@@ -44,23 +50,29 @@ public class ChoiceScreenLernmodus extends MainGUI {
         final VBox middle = new VBox(30);
         final VBox right = new VBox(20);
         final VBox bottom = new VBox(10);
+        final BorderPane paneRight = new BorderPane();
 
         middle.setId("choiceMiddleArea");
 
         middle.getChildren().addAll(  text1, text2,text3, text4);
-        right.getChildren().addAll(level1, level2, level3, level4);
+
+        right.getChildren().addAll( level1, level2, level3, level4 );
+        paneRight.setRight(getPicture());
+        paneRight.setLeft(right);
+
+
         bottom.getChildren().add(backButton);
 
         BorderPane root = new BorderPane();
 
         root.setTop(willkommensText);
-        root.setPadding(new Insets(150,370,7,20));
+        root.setPadding(new Insets(150,170,7,20));
 
         middle.setPadding(new Insets(50,7,10,7));
         right.setPadding(new Insets(50,7,10,70));
-        bottom.setPadding(new Insets(0,7,200,7));
+        bottom.setPadding(new Insets(0,7,150,7));
         root.setLeft(middle);
-        root.setRight(right);
+        root.setRight(paneRight);
         root.setBottom(bottom);
 
         return  root;
@@ -75,7 +87,35 @@ public class ChoiceScreenLernmodus extends MainGUI {
     }
 
 
+    /**
+     * Die Methode erstellt das Bild.
+     *
+     * @return Gibt den Node für das auszugebende Bild zurück.
+     */
+    public Node getPicture() {
+        try {
 
+            FileInputStream input = new FileInputStream("src/Test/Presentation/image/ChoiceScreenLernendesSmiley.png");
+
+            //prepare image object
+            Image image = new Image(input);
+
+
+            //create ImageView object
+            ImageView imageView = new ImageView(image);
+            imageView.setFitHeight(150);
+            imageView.setFitWidth(150);
+            HBox hBox = new HBox();
+            hBox.getChildren().add(imageView);
+            hBox.setPadding(new Insets(7,7,50,70));
+
+            return hBox;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
