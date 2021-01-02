0d367d9cbb85ba9608033e46ce7c2647f9479fb1
Oliver Piert <piertoli@students.zhaw.ch>
2021-01-02 13:20:10 +0100

    JavaDoc enhancements

--- src/Test/Presentation/ClockElements.java
+++ src/Test/Presentation/ClockElements.java
@@ -72,13 +72,19 @@ public class ClockElements {
     // Gruppieren von allen Ziffern für die erstellung des Uhrenbildes
     public Group ziffern = new Group (ziffer_1,ziffer_2,ziffer_3,ziffer_4,ziffer_5,ziffer_6,ziffer_7,ziffer_8,ziffer_9,ziffer_10,ziffer_11,ziffer_12);
 
-
-    /*public void createUhrKreise(){
-        // Hauptkreise Uhr
-        outerCircle = new Circle(300, 300, 160, Color.LIGHTGRAY);
-        outerCircle.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-        innerCircle = new Circle(300, 300, 6, Color.BLACK);
-    }*/
+    /**
+     * Der Konstruktor der Klasse ClockElements führt verschiedene Methoden der eigenen Klasse aus um die grafischen
+     * Elemente so bereitzustellen damit sie von der Klasse ClockSkin nur noch für den jeweiligen Spielmodus korrekt
+     * zusammengebaut werden müssen. Als zentrales Element erstellt er auch die verschiedenen Maps für die Ziffern sowie
+     * die Minuten- und Stunden-Linien.
+     */
+    public ClockElements (){
+        //createUhrKreise();
+        setupClockElements();
+        createZiffernMap();
+        createMinutenMap();
+        createStundenMap();
+    }
 
     // EventHandler auf dem Ziffernblatt sowie den einzelnen Ziffern selbst zur Änderung der Farbe des Uhrenbildes
     EventHandler<MouseEvent> eventHandler = getEventHandler();
@@ -86,7 +92,7 @@ public class ClockElements {
     /**
      * Der EventHandler steuert die zufällige Farbänderung des Ziffernblatts sowie der einzelnen Ziffern im Uhrenbild.
      * Dies ist eine versteckte Funktion im Spiel und soll beim Spieler beim Zufälligen anklicken der Uhr für eine
-     * entsprechende Überraschung sorgen.
+     * entsprechende Überraschung sorgen .
      */
     EventHandler<MouseEvent> getEventHandler() {
         //Creating the mouse event handler
@@ -106,19 +112,10 @@ public class ClockElements {
     }
 
     /**
-     * Der Konstruktor der Klasse ClockElements führt verschiedene Methoden der eigenen Klasse aus um die grafischen
-     * Elemente so bereitzustellen damit sie von der Klasse ClockSkin nur noch für den jeweiligen Spielmodus korrekt
-     * zusammengebaut werden müssen. Als zentrales Element erstellt er auch die verschiedenen Maps für die Ziffern sowie
-     * die Minuten- und Stunden-Linien.
+     * Die Methode "setupClockElements" formatiert die einzelnen Elemente des Uhrenbildes. Es werden die Nummern der
+     * Ziffern 1 bis 12 mit der Schriftart Comic Sans MS formatiert und fügt dem jeweiligen Kreis zum Event Handler
+     * hinzu. Weiter werden der äussere und innere Kreis für das Zifferblatt erstellt und dem Event Handler hinzugefügt.
      */
-    public ClockElements (){
-        //createUhrKreise();
-        setupClockElements();
-        createZiffernMap();
-        createMinutenMap();
-        createStundenMap();
-    }
-
     public void setupClockElements(){
         Font nummerFont = new Font("Comic Sans MS", 30);
 
@@ -147,15 +144,19 @@ public class ClockElements {
         nummer12.setFont(nummerFont);
         circle12.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
 
+
         outerCircle = new Circle(300, 300, 160, Color.LIGHTGRAY);
         outerCircle.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
         innerCircle = new Circle(300, 300, 6, Color.BLACK);
     }
 
-
+    /**
+     * Die Methode "createZiffernMap" erstellt eine Hashmap für die einzelnen Ziffern. Als Key wird ein Integer mit
+     * der jeweiligen Zahl der Ziffer definiert. Als Value die jeweilige Gruppe für eine Ziffer, welche aus einem Kreis
+     * und Text mit der entsprechenden Zahl besteht.
+     */
     public void createZiffernMap() {
 
-        // Kreiert ZiffernMap für mit Ziffernblätter
         ziffernMap.put(1, ziffer_1);
         ziffernMap.put(2, ziffer_2);
         ziffernMap.put(3, ziffer_3);
@@ -172,7 +173,12 @@ public class ClockElements {
         ziffernMap.put(13,ziffer_1);
     }
 
-
+    /**
+     * Die Methode "createMinutenMap" erstellt die Linien für die einzelnen Positionen des Minutenzeigers und fügt sie
+     * der Hashmap "MinutenMap" hinzu. Als Key wird ein String mit der jeweiligen anzuzeigenden Minute definiert.
+     * Als Value die jeweilige Linie für den Minutenzeiger mit den entsprechenden Koordinaten für die korrekte Zeit
+     * hinterlegt.
+     */
     public void createMinutenMap() {
 
         // Koordinaten für Minutenzeiger
@@ -187,6 +193,12 @@ public class ClockElements {
         minutenMap.put("15", minuten_15);
     }
 
+    /**
+     * Die Methode "createStundenMap" erstellt die Linien für die einzelnen Positionen des Stundenzeigers und fügt sie
+     * der Hashmap "stundenMap" hinzu. Als Key wird ein String mit der jeweiligen anzuzeigenden Stunde definiert.
+     * Als Value die jeweilige Linie für den Stundenzeiger mit den entsprechenden Koordinaten für die korrekte Zeit
+     * hinterlegt.
+     */
     public void createStundenMap() {
 
         // Linien 12 Uhr
@@ -314,4 +326,4 @@ public class ClockElements {
 
     }
 
-} // Ende Klasse
+} // Ende Klasse ClockElements
