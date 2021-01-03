d1b60d109c1dd128dde786c611101d093c252be3
Oliver Piert <piertoli@students.zhaw.ch>
2021-01-03 14:03:04 +0100

    JavaDoc enhancements

--- src/Test/Persistenz/QuestionsAnswer.java
+++ src/Test/Persistenz/QuestionsAnswer.java
@@ -1,5 +1,13 @@
+/**
+ * Die Klasse "QuestionsAnswer" erstellt für jedes Uhrenspiel-Level eine Map mit dem String der jeweils angezeigten
+ * Uhrzeit und dem String der dazugehörigen korrekten Antwort. Aufgrund dieser Maps weiss das Spiel, welche Antwort
+ * zu welcher Frage und Uhrzeit korrekt ist. Die Antworten-Map Level 1 enthält die Frage/Antwort Schlüssel für die
+ * volle Stunde, Level 2 für die halbe Stunde und Level 3 für viertel nach und viertel vor.
+ *
+ * @author Tina Kolbe & Oliver Piert
+ * @version 1.0
+ */
 package Test.Persistenz;
-
 
 
 import java.util.*;
@@ -7,9 +15,13 @@ import java.util.*;
 
 public class QuestionsAnswer {
 
+    // Instanzvariable
     public HashMap<String, String> antwortenMap;
 
-
+    /**
+     * Der Konstruktor der Klasse "QuestionsAnswer" Initialisiert die Variable "antwortenMap" vom Typ HashMap. In der
+     * Variablen werden die korrekten Frage/Antwort Schlüssel pro Level abgespeichert.
+     */
     public QuestionsAnswer() {
         antwortenMap = new HashMap<>();
 
@@ -17,64 +29,75 @@ public class QuestionsAnswer {
 
     }
 
+    /**
+     * Die Mehtode "antwortenMapLevel1" befüllt die "antwortenMap" vom Typ HashMap mit den korrekten Frage/Antwort
+     * Schlüssel für Level 1 (volle Stunde).
+     */
     public void antwortenMapLevel1() {
-        antwortenMap.put("01:00","1:00");
-        antwortenMap.put("02:00","2:00");
-        antwortenMap.put("03:00","3:00");
-        antwortenMap.put("04:00","4:00");
-        antwortenMap.put("05:00","5:00");
-        antwortenMap.put("06:00","6:00");
-        antwortenMap.put("07:00","7:00");
-        antwortenMap.put("08:00","8:00");
-        antwortenMap.put("09:00","9:00");
-        antwortenMap.put("10:00","10:00");
-        antwortenMap.put("11:00","11:00");
-        antwortenMap.put("12:00","12:00");
+        antwortenMap.put("01:00", "1:00");
+        antwortenMap.put("02:00", "2:00");
+        antwortenMap.put("03:00", "3:00");
+        antwortenMap.put("04:00", "4:00");
+        antwortenMap.put("05:00", "5:00");
+        antwortenMap.put("06:00", "6:00");
+        antwortenMap.put("07:00", "7:00");
+        antwortenMap.put("08:00", "8:00");
+        antwortenMap.put("09:00", "9:00");
+        antwortenMap.put("10:00", "10:00");
+        antwortenMap.put("11:00", "11:00");
+        antwortenMap.put("12:00", "12:00");
     }
 
 
-
+    /**
+     * Die Mehtode "antwortenMapLevel2" befüllt die "antwortenMap" vom Typ HashMap mit den korrekten Frage/Antwort
+     * Schlüssel für Level 2 (halbe Stunde).
+     */
     public void antwortenMapLevel2() {
-        antwortenMap.put("01:30","1:30");
-        antwortenMap.put("02:30","2:30");
-        antwortenMap.put("03:30","3:30");
-        antwortenMap.put("04:30","4:30");
-        antwortenMap.put("05:30","5:30");
-        antwortenMap.put("06:30","6:30");
-        antwortenMap.put("07:30","7:30");
-        antwortenMap.put("08:30","8:30");
-        antwortenMap.put("09:30","9:30");
-        antwortenMap.put("10:30","10:30");
-        antwortenMap.put("11:30","11:30");
-        antwortenMap.put("12:30","12:30");
+        antwortenMap.put("01:30", "1:30");
+        antwortenMap.put("02:30", "2:30");
+        antwortenMap.put("03:30", "3:30");
+        antwortenMap.put("04:30", "4:30");
+        antwortenMap.put("05:30", "5:30");
+        antwortenMap.put("06:30", "6:30");
+        antwortenMap.put("07:30", "7:30");
+        antwortenMap.put("08:30", "8:30");
+        antwortenMap.put("09:30", "9:30");
+        antwortenMap.put("10:30", "10:30");
+        antwortenMap.put("11:30", "11:30");
+        antwortenMap.put("12:30", "12:30");
     }
+
+    /**
+     * Die Mehtode "antwortenMapLevel3" befüllt die "antwortenMap" vom Typ HashMap mit den korrekten Frage/Antwort
+     * Schlüssel für Level 3 (viertel nach und viertel vor).
+     */
     public void antwortenMapLevel3() {
-        antwortenMap.put("01:15","1:15");
-        antwortenMap.put("02:15","2:15");
-        antwortenMap.put("03:15","3:15");
-        antwortenMap.put("04:15","4:15");
-        antwortenMap.put("05:15","5:15");
-        antwortenMap.put("06:15","6:15");
-        antwortenMap.put("07:15","7:15");
-        antwortenMap.put("08:15","8:15");
-        antwortenMap.put("09:15","9:15");
-        antwortenMap.put("10:15","10:15");
-        antwortenMap.put("11:15","11:15");
-        antwortenMap.put("12:15","12:15");
-        antwortenMap.put("01:45","1:45");
-        antwortenMap.put("02:45","2:45");
-        antwortenMap.put("03:45","3:45");
-        antwortenMap.put("04:45","4:45");
-        antwortenMap.put("05:45","5:45");
-        antwortenMap.put("06:45","6:45");
-        antwortenMap.put("07:45","7:45");
-        antwortenMap.put("08:45","8:45");
-        antwortenMap.put("09:45","9:45");
-        antwortenMap.put("10:45","10:45");
-        antwortenMap.put("11:45","11:45");
-        antwortenMap.put("12:45","12:45");
+        antwortenMap.put("01:15", "1:15");
+        antwortenMap.put("02:15", "2:15");
+        antwortenMap.put("03:15", "3:15");
+        antwortenMap.put("04:15", "4:15");
+        antwortenMap.put("05:15", "5:15");
+        antwortenMap.put("06:15", "6:15");
+        antwortenMap.put("07:15", "7:15");
+        antwortenMap.put("08:15", "8:15");
+        antwortenMap.put("09:15", "9:15");
+        antwortenMap.put("10:15", "10:15");
+        antwortenMap.put("11:15", "11:15");
+        antwortenMap.put("12:15", "12:15");
+        antwortenMap.put("01:45", "1:45");
+        antwortenMap.put("02:45", "2:45");
+        antwortenMap.put("03:45", "3:45");
+        antwortenMap.put("04:45", "4:45");
+        antwortenMap.put("05:45", "5:45");
+        antwortenMap.put("06:45", "6:45");
+        antwortenMap.put("07:45", "7:45");
+        antwortenMap.put("08:45", "8:45");
+        antwortenMap.put("09:45", "9:45");
+        antwortenMap.put("10:45", "10:45");
+        antwortenMap.put("11:45", "11:45");
+        antwortenMap.put("12:45", "12:45");
     }
 
 
-
-}
+} // Ende Klasse QuestionsAnswer
--- src/Test/Persistenz/SavedData.java
+++ src/Test/Persistenz/SavedData.java
@@ -1,3 +1,10 @@
+/**
+ * Die Klasse "SavedData" ist für das Speichern und Landen von Uhrenspiel Spielständen zuständig. Dazu wird über
+ * das Interface "IOInterface" auf die benötigten "save" und "load" Methoden zugegriffen.
+ *
+ * @author Tina Kolbe & Oliver Piert
+ * @version 1.0
+ */
 package Test.Persistenz;
 
 import javafx.stage.FileChooser;
@@ -9,32 +16,64 @@ import java.util.*;
 
 public class SavedData {
 
+    // Instanzvariablen
     private IOInterface bufferInterface;
     private Random random;
     public List<String> progress;
 
 
+    /**
+     * Der Konstruktor der Klasse "SavedData" Initialisiert die Instanzvariablen als Objekte.
+     */
     public SavedData() {
         progress = new ArrayList();
         random = new Random();
         bufferInterface = new FileWriter();
     }
 
+    /**
+     * Die Methode "setProgress" stellt sicher, dass die Liste von Strings "progress" geleert wird bevor diese mit
+     * dem neuen abzuspeichernden Spielvortschritt befüllt wird.
+     *
+     * @param newprogress Der Parameter enthält eine Liste von Strings mit dem neu abzuspeichernden Spielvortschritt.
+     */
     public void setProgress(List<String> newprogress) {
         this.progress.clear();
         this.progress.addAll(newprogress);
     }
 
-
+    /**
+     * Die Methode "saveProgress" nutzt das IOInterface und übergibt die zur Speicherung ddes Files benötigten Parameter
+     * der Methode "save".
+     *
+     * @param file Der Parameter wird im Interface "IOInterface" der "save" Methode übergeben und vom FileWriter
+     *             verwendet um das File abzuspeichern.
+     * @param list Der Parameter wird im Interface "IOInterface" der "save" Methode übergeben und vom BufferedWriter
+     *             verwendet um die abzuspeichernden Informationen in das zu speichernde File zu schreiben.
+     */
     public void saveProgress(File file, List<String> list) throws IOException {
         bufferInterface.save(file, list);
     }
 
+    /**
+     * Die Methode "loadProgress" nutzt das IOInterface und übergibt den zum Laden des Files benötigten Parameter
+     * der Methode "load".
+     *
+     * @param file Der Parameter wird im Interface "IOInterface" der "load" Methode übergeben und vom FileReader
+     *             verwendet um das File zu Laden.
+     */
     public void loadProgress(File file) throws ClassNotFoundException, IOException {
         setProgress(bufferInterface.load(file));
         System.out.println("Das Spiel wurde geladen: " + progress);
     }
 
+    /**
+     * Die Methode "chooseSaveFile" erstellt mit FileChooser das "Speichere Spiel" Dialogfenster
+     *
+     * @param file Der Parameter übergibt das zu speichernde File dem FileChooser.
+     * @param stage Der Parameter übergibt die Stage die zur Darstellung Dialogfensters benötigt wird.
+     * @return Die Methode gibt das abzuspeichernte File zurück.
+     */
     public File chooseSaveFile(File file, Stage stage) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Speichere Spiel");
@@ -49,6 +88,13 @@ public class SavedData {
         return fileChooser.showSaveDialog(stage);
     }
 
+    /**
+     * Die Methode "chooseLoadFile" erstellt mit FileChooser das "Lade Spiel" Dialogfenster
+     *
+     * @param file Der Parameter übergibt das zu ladende File dem FileChooser.
+     * @param stage Der Parameter übergibt die Stage die zur Darstellung Dialogfensters benötigt wird.
+     * @return Die Methode gibt das zu ladende File zurück.
+     */
     public File chooseLoadFile(File file, Stage stage) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Lade Spiel");
@@ -61,5 +107,6 @@ public class SavedData {
         // Show open file dialog
         return fileChooser.showOpenDialog(stage);
     }
-}
+
+} // Ende Klasse SavedData
 
